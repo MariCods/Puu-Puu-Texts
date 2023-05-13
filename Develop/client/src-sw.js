7bd4235fa4e6b49253365925d0b8060fb38c73bb
Mary Cods <maryam.selassie@gmail.com>
2023-05-13 16:25:48 -0700

    SRC Update

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate  } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -16,6 +16,7 @@ const pageCache = new CacheFirst({
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
+   
   ],
 });
 
@@ -24,11 +25,12 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
+registerRoute(({ request }) => request.mode === 'navigate', pageCache
+);
 // TODO: Implement asset caching
 registerRoute(
-  new offlineFallback({
+({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
     cacheName: 'asset-cache',
-  })
-);
+})
+)
